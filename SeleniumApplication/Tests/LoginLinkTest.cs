098d4dbbb37979ba87ce0123f1075ffc92e99f34
lionvs <vsyn@eliftech.com>
2018-02-24 12:16:19 +0200

    Created initial project

---
+++ SeleniumApplication/Tests/LoginLinkTest.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace SeleniumApplication.Tests
+{
+    public class LoginLinkTest
+    {
+        IWebDriver Browser;
+
+        [SetUp]
+        public void Init()
+        {
+            Browser = new ChromeDriver();
+            Browser.Url = "http://www.justanswer.co.uk";
+        }
+
+        [Test(Description ="Test clicking 'Login' link on the page redirects to correct page and login button is displayed there")]
+        public void ClickLoginLink()
+        {
+            //Arrange
+            IWebElement loginLink = Browser.FindElement(By.CssSelector("#JA_HeaderLogin"));
+
+            //Act
+            loginLink.Click();
+
+            //Assert
+            IWebElement expectedLoginButton = Browser.FindElement(By.CssSelector("#ctl00_BodyContent_btnLogin"));
+            Assert.AreEqual("https://secure.justanswer.co.uk/login.aspx", Browser.Url);
+            Assert.IsTrue(expectedLoginButton.Displayed);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Browser.Close();
+        }
+    }
+}
